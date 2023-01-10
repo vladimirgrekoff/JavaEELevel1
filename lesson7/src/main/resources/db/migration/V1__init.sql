fed991aa12caa47b526616aa2268e1930f10c493
Vladimir Grekov <grekoff_vv@mail.ru>
2023-01-11 05:03:42 +0700

    homework7 update

--- lesson7/src/main/resources/db/migration/V1__init.sql
+++ lesson7/src/main/resources/db/migration/V1__init.sql
@@ -1,10 +1,3 @@
-create table categories (id bigserial primary key, title varchar(255));
-insert into categories (title)
-values
-('Food');
-
-
-
 create table products (id bigserial primary key, title varchar(255), price int, category_id bigint references categories (id));
 insert into products (title, price, category_id)
 values
